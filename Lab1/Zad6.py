11dc47133e355fc21131a5ddc735944bbfee34e6
szymonlibicki73266 <73266@student.wsiz.edu.pl>
2025-10-28 12:07:03 +0100

    zadania

--- Lab1/Zad6.py
+++ Lab1/Zad6.py
@@ -2,3 +2,5 @@ cenapaliwa = float(6.5)
 trasa = float(input("podaj dlugosc trasy:"))
 spalanie = float(input("podaj srednie spalanie:"))
 
+cena = (trasa/100)*spalanie*cenapaliwa
+print("Koszt trasy:",cena)
